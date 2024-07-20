3cf406dd7634f3a9edaf7f24d599a3a6b8acf054
rahulc97 <rahul.c@keyvalue.systems>
2024-07-20 19:50:08 +0530

    style issue in build

--- src/components/ShareThoughts.jsx
+++ src/components/ShareThoughts.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const ShareThoughts = () => {
   return (
     <div className='bg-market h-[734px] bg-cover relative overflow-hidden py-[80px]'>
-      <StaticImage src="../images/phone.png" objectFit='contain' className='h-[300px] w-[300px] md:h-[450px] md:w-[450px] lg:h-[550px] lg:w-[550px] absolute bottom-0 -left-[100px] min-[485px]-left-[0%] xl:left-[15%] lg:-mb-[20px]' />
+      <StaticImage src="../images/phone.png" objectFit='contain' className='h-[300px] w-[300px] md:h-[450px] md:w-[450px] lg:h-[550px] lg:w-[550px] !absolute bottom-0 -left-[100px] min-[485px]-left-[0%] xl:left-[15%] lg:-mb-[20px]' />
       <div className='text-white absolute top-[50%] -translate-y-[50%] right-0 sm:right-[25%] sm:translate-x-[50%] mx-[40px] xl:mx-[80px] w-[35%] max-w-[386px]'>
         <div className='text-[24px] sm:text-[36px]'>
           Share your thoughts with family and friends
